be5c63601636453694c3c1b45d488fd894e8858b
Nithish Raghunandanan <nithish.raghunandanan@couchbase.com>
2021-06-24 22:02:03 +0200

    Linting; minor updates; convert tests to Pytests

--- src/app.py
+++ src/app.py
@@ -1,9 +1,5 @@
-'''
-python -m pip install couchbase
-python -m pip install flask
-python -m pip install bcrypt
-python -m pip install python-dotenv
-python -m pip install flask-restx
+"""
+python -m pip install -r requirements.txt
 
 export FLASK_APP=src/app && \
 export FLASK_ENV=development
@@ -12,28 +8,30 @@ NOTE:  make sure to change into the src
     directory before running flask
 
 flask run
-'''
-#setup flask
-from flask import Flask, jsonify, request
-from flask_restx import Resource, Api, fields
-from codecs import decode
-
-#setup couchbase
-from couchbase.management.buckets import BucketSettings 
-from couchbase.cluster import Cluster, ClusterOptions
-from couchbase.auth import PasswordAuthenticator
-from couchbase.exceptions import *
-from couchbase.management.collections import CollectionSpec
-from couchbase.diagnostics import PingState
-
-from datetime import datetime
-
-import uuid 
+"""
 import time
+import uuid
+from codecs import decode
+from datetime import datetime
+import os
+from attr import validate
 import bcrypt
+from dotenv import load_dotenv
+
+# setup couchbase
+from couchbase.auth import PasswordAuthenticator
+from couchbase.cluster import Cluster, ClusterOptions
+from couchbase.diagnostics import PingState
+from couchbase.exceptions import *
+from couchbase.management.buckets import BucketSettings
+from couchbase.management.collections import CollectionSpec
+
+# setup flask
+from flask import Flask, jsonify, request
+from flask_restx import Api, Resource, fields
+
 
 class CouchbaseClient(object):
-
     @classmethod
     def create_client(_, *args, **kwargs):
         self = CouchbaseClient(*args)
@@ -44,13 +42,13 @@ class CouchbaseClient(object):
 
     _instance = None
 
-    def __new__(cls, host, bucket, collection, scope, username, pw):
+    def __new__(cls, host, bucket, scope, collection, username, pw):
         if CouchbaseClient._instance is None:
             CouchbaseClient._instance = object.__new__(cls)
             CouchbaseClient._instance.host = host
             CouchbaseClient._instance.bucket_name = bucket
             CouchbaseClient._instance.collection_name = collection
-            CouchbaseClient._instance.scope_name = scope 
+            CouchbaseClient._instance.scope_name = scope
             CouchbaseClient._instance.username = username
             CouchbaseClient._instance.password = pw
         return CouchbaseClient._instance
@@ -58,54 +56,54 @@ class CouchbaseClient(object):
     def connect(self, **kwargs):
         # note: kwargs would be how one could pass in
         #       more info for client config
-        conn_str = 'couchbase://{0}'.format(self.host)
+        conn_str = f"couchbase://{self.host}"
 
         try:
             cluster_opts = ClusterOptions(
-                authenticator=PasswordAuthenticator(
-                    self.username, self.password))
-            self._cluster = Cluster(conn_str, options=cluster_opts)
-
+                authenticator=PasswordAuthenticator(self.username, self.password)
+            )
+            self._cluster = Cluster(conn_str, options=cluster_opts, **kwargs)
         except CouchbaseException as error:
-            print('Could not connect to cluster. Error: {}'.format(error))
+            print(f"Could not connect to cluster. Error: {error}")
             raise
 
         try:
-            #create bucket if it doesn't exist
-            bucketSettings = BucketSettings(name = self.bucket_name, ram_quota_mb=256)
+            # create bucket if it doesn't exist
+            bucketSettings = BucketSettings(name=self.bucket_name, ram_quota_mb=256)
             self._cluster.buckets().create_bucket(bucketSettings)
         except BucketAlreadyExistsException:
-            print('bucket already exists')
+            print("Bucket already exists")
 
         self._bucket = self._cluster.bucket(self.bucket_name)
 
         try:
-            #create collection if it doesn't exist
-            colSpec = CollectionSpec(self.collection_name, self.scope_name) 
+            # create collection if it doesn't exist
+            colSpec = CollectionSpec(self.collection_name, self.scope_name)
             self._bucket.collections().create_collection(colSpec)
-            
+
         except CollectionAlreadyExistsException:
-            print ('collection already exists')
+            print("Collection already exists")
 
         self._collection = self._bucket.collection(self.collection_name)
 
-
-        #create index if it doesn't exist
-        time.sleep(5)
+        # create index if it doesn't exist
+        # sleep to ensure that the operations are finished before trying to create the index
+        time.sleep(6)
         try:
-            #create index if it doesn't exist
-            createIndexProfile = "CREATE PRIMARY INDEX default_profile_index ON " + self.bucket_name + "." + self.scope_name + "." + self.collection_name 
-            createIndex = "CREATE PRIMARY INDEX ON " + self.bucket_name 
+            # create index if it doesn't exist
+            createIndexProfile = f"CREATE PRIMARY INDEX default_profile_index ON {self.bucket_name}.{self.scope_name}.{self.collection_name}"
+            createIndex = f"CREATE PRIMARY INDEX ON {self.bucket_name}"
 
             self._bucket.query(createIndexProfile).execute()
             self._bucket.query(createIndex).execute()
         except QueryIndexAlreadyExistsException:
-            print ('index already exists')
+            print("Index already exists")
         except Exception as e:
-            print (f"Error: {e}")
+            print(f"Error: {e}")
 
     def ping(self):
         try:
+            # check the health of cluster services
             # if couchbase version >= 3.0.10:
             # else use self._bucket.ping()
             result = self._cluster.ping()
@@ -130,25 +128,39 @@ class CouchbaseClient(object):
     def remove(self, key):
         return self._collection.remove(key)
 
-    def query(self, strQuery):
-        return self._bucket.query(strQuery)
+    def query(self, strQuery, *options, **kwargs):
+        # options are used for positional parameters
+        # kwargs are used for named parameters
 
-#setup API endpoints
+        # bucket.query() is different from cluster.query()
+        return self._cluster.query(strQuery, *options, **kwargs)
 
-#setup app and api's using Flask
+
+# setup API endpoints
+
+# setup app and APIs using Flask
 app = Flask(__name__)
-api = Api(app, version="1.0", title="Python API Quickstart - Profile", description="Couchbase Quickstart API with Python, Flask, and Flask-RestX")
-nsHealthCheck = api.namespace("api/v1/healthcheck", description="Sanity check for unit tests")
+api = Api(
+    app,
+    version="1.0",
+    title="Python API Quickstart - Profile",
+    description="Couchbase Quickstart API with Python, Flask, and Flask-RestX",
+)
+nsHealthCheck = api.namespace(
+    "api/v1/healthcheck", description="Sanity check for unit tests"
+)
 nsProfile = api.namespace("api/v1/profile", "CRUD operations for Profile")
 
-#setup models for post, put, search
+# setup models for post, put, search
 profileInsert = api.model(
     "ProfileInsert",
     {
         "firstName": fields.String(required=False, description="User First Name"),
-        "lastName": fields.String(required=False, description="User First Name"),
+        "lastName": fields.String(required=False, description="User Last Name"),
         "email": fields.String(required=True, description="User Email Address"),
-        "password": fields.String(required=True, description="User password unecrypted")
+        "password": fields.String(
+            required=True, description="User Password Unecrypted"
+        ),
     },
 )
 
@@ -157,129 +169,177 @@ profile = api.model(
     {
         "pid": fields.String(required=True, description="ID of the document (UUID)"),
         "firstName": fields.String(required=False, description="User First Name"),
-        "lastName": fields.String(required=False, description="User First Name"),
+        "lastName": fields.String(required=False, description="User Last Name"),
         "email": fields.String(required=True, description="User Email Address"),
-        "password": fields.String(required=True, description="User password unecrypted")
+        "password": fields.String(
+            required=True, description="User Password Unecrypted"
+        ),
     },
 )
 
-@nsHealthCheck.route('')
+
+@nsHealthCheck.route("/")
 class HealthCheck(Resource):
     # tag::get[]
     def get(self):
-        return "{\"time\": \"" + datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + "\"}"
-    #end::get[]
+        return jsonify(time=datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
 
-@nsProfile.route('')
+    # end::get[]
+
+
+@nsProfile.route("")
 class Profile(Resource):
     # tag::post[]
-    @nsProfile.doc("Create Profile", reponses={ 201: 'Created', 409: 'Key alreay exists', 500: 'Unexpected Error' })
-    @nsProfile.expect(profileInsert)
+    @nsProfile.doc(
+        "Create Profile",
+        reponses={201: "Created", 409: "Key alreay exists", 500: "Unexpected Error"},
+    )
+    @nsProfile.expect(profileInsert, validate=True)
     @nsProfile.marshal_with(profile)
     def post(self):
         try:
             data = request.json
-            #create new random key
+            # create new random key
             key = uuid.uuid4().__str__()
             data["pid"] = key
-            #encrypt password
-            hashed = bcrypt.hashpw(data["password"].encode('utf-8'), bcrypt.gensalt()).decode()
+            # encrypt password
+            hashed = bcrypt.hashpw(
+                data["password"].encode("utf-8"), bcrypt.gensalt()
+            ).decode()
             data["password"] = hashed
             cb.insert(key, data)
             return data, 201
         except DocumentExistsException:
-            return 'Key already exists', 409
+            return "Key already exists", 409
         except CouchbaseException as e:
-            return 'Unexpected error: {}'.format(e), 500
+            return f"Unexpected error: {e}", 500
         except Exception as e:
-            return 'Unexpected error: {}'.format(e), 500
+            return f"Unexpected error: {e}", 500
+
     # end::post[]
 
-@nsProfile.route('/<id>')
+
+@nsProfile.route("/<id>")
 class ProfileId(Resource):
     # tag::get[]
-    @nsProfile.doc("Get Profile", reponses={200: 'Found document', 404: 'Key not found', 500: 'Unexpected Error'})
+    @nsProfile.doc(
+        "Get Profile",
+        reponses={200: "Found document", 404: "Key not found", 500: "Unexpected Error"},
+    )
     def get(self, id):
         try:
             res = cb.get(id)
-            return jsonify(res.content_as[dict])  
+            return jsonify(res.content_as[dict])
         except DocumentNotFoundException:
-            return 'Key not found', 404
+            return "Key not found", 404
         except CouchbaseException as e:
-            return 'Unexpected error: {}'.format(e), 500
-    # end::get[]    
+            return f"Unexpected error: {e}", 500
+
+    # end::get[]
 
     # tag::put[]
-    @nsProfile.doc("Update Profile", reponses={200: 'Document updated', 404: 'Document not found', 500: 'Unexpected Error'})
+    @nsProfile.doc(
+        "Update Profile",
+        reponses={
+            200: "Document updated",
+            404: "Document not found",
+            500: "Unexpected Error",
+        },
+    )
     @nsProfile.expect(profileInsert)
     @nsProfile.marshal_with(profile)
-    def put(self,id):
+    def put(self, id):
         try:
             data = request.json
-            #create new random key
-            data["pid"] = id 
-            #encrypt password
-            hashed = bcrypt.hashpw(data["password"].encode('utf-8'), bcrypt.gensalt()).decode()
+            # create new random key
+            data["pid"] = id
+            # encrypt password
+            hashed = bcrypt.hashpw(
+                data["password"].encode("utf-8"), bcrypt.gensalt()
+            ).decode()
             data["password"] = hashed
             cb.upsert(id, data)
-            return data 
+            return data
         except DocumentNotFoundException:
             # Document already deleted / never existed
-            return 'Key does not exist', 404
+            return "Key does not exist", 404
         except CouchbaseException as e:
-            return 'Unexpected error: {}'.format(e), 500
+            return f"Unexpected error: {e}", 500
         except Exception as e:
-            return 'Unexpected error: {}'.format(e), 500
+            return f"Unexpected error: {e}", 500
+
     # end::put[]
 
     # tag::delete[]
-    @nsProfile.doc("Delete Profile", reponses={200: 'Delete document', 404: 'Document not found', 500: 'Unexpected Error'})
-    def delete(self,id):
+    @nsProfile.doc(
+        "Delete Profile",
+        reponses={
+            200: "Delete document",
+            404: "Document not found",
+            500: "Unexpected Error",
+        },
+    )
+    def delete(self, id):
         try:
             cb.remove(id)
-            return 'OK'
+            return "OK"
         except DocumentNotFoundException:
             # Document already deleted / never existed
-            return 'Key does not exist', 404
+            return "Key does not exist", 404
         except Exception as e:
-            return 'Unexpected error: {}'.format(e), 500
+            return f"Unexpected error: {e}", 500
+
     # end::delete[]
 
 
 # tag::get[]
-@nsProfile.route('/profiles')
+@nsProfile.route("/profiles")
 class Profiles(Resource):
-    @nsProfile.doc("Find Profiles", reponses={ 201: 'found', 500: 'Unexpected Error' }, params= {'search':'value to search for', 'limit': 'int value of number of records to return', 'skip': 'int value of how many documents to skip'})
+    @nsProfile.doc(
+        "Find Profiles",
+        reponses={201: "found", 500: "Unexpected Error"},
+        params={
+            "search": "value to search for",
+            "limit": "int value of number of records to return",
+            "skip": "int value of how many documents to skip",
+        },
+    )
     def get(self):
         try:
-            #get vars from GET request
+            # get vars from GET request
             search = request.args.get("search")
-            limit = request.args.get("limit")
-            skip = request.args.get("skip")
+            limit = int(request.args.get("limit", 5))
+            skip = int(request.args.get("skip", 0))
 
-            #create query
-            query = f"SELECT p.* FROM  {db_info['bucket']}.{db_info['scope']}.{db_info['collection']} p WHERE lower(p.firstName) LIKE '%{search.lower()}%' OR lower(p.lastName) LIKE '%{search.lower()}%' LIMIT {limit} OFFSET {skip}"
-            res = cb.query(query)
+            search_str = f"%{search.lower()}%"
 
-            #must loop through results
-            #https://docs.couchbase.com/python-sdk/current/howtos/n1ql-queries-with-sdk.html#streaming-large-result-sets
-            profiles = []
-            for x in res:
-                profiles.append(x)
-            return jsonify(profiles)  
+            # create query
+            query = f"SELECT p.* FROM  {db_info['bucket']}.{db_info['scope']}.{db_info['collection']} p WHERE lower(p.firstName) LIKE $search_str OR lower(p.lastName) LIKE $search_str LIMIT $limit OFFSET $offset;"
+            print(query)
+            res = cb.query(query, limit=limit, offset=skip, search_str=search_str)
+
+            # must loop through results
+            # https://docs.couchbase.com/python-sdk/current/howtos/n1ql-queries-with-sdk.html#streaming-large-result-sets
+            profiles = [x for x in res]
+
+            return jsonify(profiles)
+        # except
         except CouchbaseException as e:
-            return 'Unexpected error: {}'.format(e), 500
-    # end::get[]   
+            return f"Unexpected error: {e}", 500
 
+    # end::get[]
+
+
+load_dotenv()
 # done for example purposes only, some
 # sort of configuration should be used
 db_info = {
-    'host': 'localhost',
-    'bucket': 'user_profile',
-    'collection': 'profile',
-    'scope': '_default',
-    'username': 'Administrator',
-    'password': 'password'
+    "host": os.getenv("DB_HOST"),
+    "bucket": os.getenv("BUCKET"),
+    "scope": os.getenv("SCOPE"),
+    "collection": os.getenv("COLLECTION"),
+    "username": os.getenv("USERNAME"),
+    "password": os.getenv("PASSWORD"),
 }
 
 cb = CouchbaseClient.create_client(*db_info.values())
--- src/test.py
+++ src/test.py
@@ -1,57 +1,191 @@
 import requests
+import pytest
 
-# ##############################
-# TODO:  make real unit tests  #
-# ##############################
+"""How to Run Unit Tests
+pytest test.py"""
 
 BASE = "http://127.0.0.1:5000"
-BASEURI = BASE + "/api/v1/profile/"
-BASESEARCHURI = BASE + "/api/v1/profiles"
-
-profileData = {
-    "firstName": "john", 
-    "lastName": "doe", 
-    "email": "john.doe@couchbase.com", 
-    "password": "password"
-}
-
-response = requests.get(BASE + "/api/v1/healthCheck/")
-responsePost = requests.post(url=BASEURI, json=profileData)
-
-print("healthCheck:\n")
-print("------------\n")
-print (response.json())
-print ("\n")
-
-print("add profile:\n")
-print("------------\n")
-print (responsePost.json())
-print ("\n")
-
-#transform data with update
-data = responsePost.json()
-data["firstName"] = "Jane"
-data["lastName"] = "Doe"
-data["email"] = "jane.doe@couchbase.com"
-data["password"] = "Password1"
-postUri = BASEURI + data["pid"]
-responsePut = requests.put(url=postUri, json=data)
-
-print("update profile:\n")
-print("------------\n")
-print (responsePut)
-print ("\n")
+BASEURI = f"{BASE}/api/v1/profile"
+BASESEARCHURI = f"{BASE}/api/v1/profile/profiles"
 
 
-searchUri = f"{BASESEARCHURI}?search=jane&limit=5&skip=0"
-print ("URL for Search: " + searchUri)
-responseSearch = requests.get(url=searchUri)
-print("------------\n")
-print (responseSearch.json())
-print ("\n")
+def test_health_check():
+    """Test the health check end point"""
+    response = requests.get(f"{BASE}/api/v1/healthcheck/")
+    assert response.status_code == 200
 
-responseDelete = requests.delete(url=postUri)
-print("delete profile:\n")
-print("------------\n")
-print (responseDelete)
-print ("\n")
+
+def test_add_profile():
+    """Test the successful creation of a user profile"""
+    profile_data = {
+        "firstName": "john",
+        "lastName": "doe",
+        "email": "john.doe@couchbase.com",
+        "password": "password",
+    }
+    response = requests.post(url=BASEURI, json=profile_data)
+    assert response.status_code == 201
+    response_data = response.json()
+    assert response_data["firstName"] == "john"
+    assert response_data["lastName"] == "doe"
+    assert response_data["email"] == "john.doe@couchbase.com"
+
+
+def test_add_profile_without_email():
+    """Test the creation of a user profile without email"""
+    profile_data = {
+        "firstName": "john",
+        "lastName": "doe",
+        "password": "password",
+    }
+    response = requests.post(url=BASEURI, json=profile_data)
+    assert response.status_code == 400
+    response_data = response.json()
+    assert response_data["errors"]["email"] == "'email' is a required property"
+
+
+def test_add_profile_without_password():
+    """Test the creation of a user profile without password"""
+    profile_data = {
+        "firstName": "john",
+        "lastName": "doe",
+        "email": "john.doe@couchbase.com",
+    }
+    response = requests.post(url=BASEURI, json=profile_data)
+    assert response.status_code == 400
+    response_data = response.json()
+    assert response_data["errors"]["password"] == "'password' is a required property"
+
+
+def test_add_profile_without_email_and_password():
+    """Test the creation of a user profile without email and password"""
+    profile_data = {"firstName": "john", "lastName": "doe"}
+    response = requests.post(url=BASEURI, json=profile_data)
+    assert response.status_code == 400
+    response_data = response.json()
+    assert response_data["errors"]["email"] == "'email' is a required property"
+    assert response_data["errors"]["password"] == "'password' is a required property"
+
+
+def test_get_user_profile_id():
+    """Test getting a user profile by id"""
+    profile_data = {
+        "firstName": "john",
+        "lastName": "doe",
+        "email": "john.doe@couchbase.com",
+        "password": "password",
+    }
+    response_creation = requests.post(url=BASEURI, json=profile_data)
+    assert response_creation.status_code == 201
+    id = response_creation.json()["pid"]
+
+    response_get = requests.get(url=f"{BASEURI}/{id}")
+    assert response_get.status_code == 200
+    read_result = response_get.json()
+    assert read_result["firstName"] == profile_data["firstName"]
+    assert read_result["lastName"] == profile_data["lastName"]
+    assert read_result["email"] == profile_data["email"]
+
+
+def test_get_user_profile__invalid_id():
+    """Test getting a user profile by invalid id"""
+    id = 1234
+
+    response_get = requests.get(url=f"{BASEURI}/{id}")
+    assert response_get.status_code == 404
+    assert response_get.text == '"Key not found"\n'
+
+
+def test_update_user_profile():
+    """Test updating a user profile"""
+    profile_data = {
+        "firstName": "john",
+        "lastName": "doe",
+        "email": "john.doe@couchbase.com",
+        "password": "password",
+    }
+    response_creation = requests.post(url=BASEURI, json=profile_data)
+    assert response_creation.status_code == 201
+    id = response_creation.json()["pid"]
+
+    updated_profile_data = {
+        "firstName": "jane",
+        "lastName": "doe",
+        "email": "jane.doe@couchbase.com",
+        "password": "password#",
+    }
+    response_update = requests.put(url=f"{BASEURI}/{id}", json=updated_profile_data)
+
+    assert response_update.status_code == 200
+    updated_profile = response_update.json()
+
+    assert updated_profile["firstName"] == updated_profile_data["firstName"]
+    assert updated_profile["lastName"] == updated_profile_data["lastName"]
+    assert updated_profile["email"] == updated_profile_data["email"]
+
+
+def test_delete_user_profile():
+    """Test deleting a user profile"""
+    profile_data = {
+        "firstName": "john",
+        "lastName": "doe",
+        "email": "john.doe@couchbase.com",
+        "password": "password",
+    }
+    response_creation = requests.post(url=BASEURI, json=profile_data)
+    assert response_creation.status_code == 201
+    id = response_creation.json()["pid"]
+
+    response_delete = requests.delete(url=f"{BASEURI}/{id}")
+
+    assert response_delete.status_code == 200
+    assert response_delete.text == '"OK"\n'
+
+
+def test_delete_invalid_user_profile():
+    """Test deleting an invalid user profile"""
+    id = 1234
+
+    response_delete = requests.delete(url=f"{BASEURI}/{id}")
+
+    assert response_delete.status_code == 404
+    assert response_delete.text == '"Key does not exist"\n'
+
+
+def test_search_match():
+    """Test searching for a user profile that matches the search string"""
+    profile_data = {
+        "firstName": "john",
+        "lastName": "doe",
+        "email": "john.doe@couchbase.com",
+        "password": "password",
+    }
+    response_creation = requests.post(url=BASEURI, json=profile_data)
+    assert response_creation.status_code == 201
+
+    search_term = "jo"
+    search_response = requests.get(url=f"{BASESEARCHURI}?search={search_term}")
+    assert search_response.status_code == 200
+    results = search_response.json()
+
+    # check if the results returned contain the search term in them
+    for res in results:
+        assert not (
+            res["firstName"].find(search_term) == -1
+            and res["lastName"].find(search_term) == -1
+        )
+
+
+def test_search_no_match():
+    """Test searching for a user profile that does not match the search string"""
+    search_term = "123"
+    search_response = requests.get(url=f"{BASESEARCHURI}?search={search_term}")
+    assert search_response.status_code == 200
+    results = search_response.json()
+
+    # check if the results returned contain the search term in them
+    for res in results:
+        assert (
+            res["firstName"].find(search_term) == -1
+            and res["lastName"].find(search_term) == -1
+        )
