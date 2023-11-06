15fe6cbd27ea960eac0ec177d31f9642baa69036
Nithish Raghunandanan <nithish.raghunandanan@couchbase.com>
2023-11-06 19:17:50 +0100

    Update app description

--- src/app.py
+++ src/app.py
@@ -17,7 +17,25 @@ app.config["RESTX_MASK_SWAGGER"] = False
 api = Api(
     title="Python Quickstart using Flask",
     version="1.0",
-    description="A quickstart API using Python with Couchbase, Flask & travel-sample data",
+    description="""
+    A quickstart API using Python with Couchbase, Flask & travel-sample data.
+
+    We have a visual representation of the API documentation using Swagger which allows you to interact with the API's endpoints directly through the browser. It provides a clear view of the API including endpoints, HTTP methods, request parameters, and response objects. 
+    
+    Click on an individual endpoint to expand it and see detailed information. This includes the endpoint's description, possible response status codes, and the request parameters it accepts.
+
+    Trying Out the API
+    You can try out an API by clicking on the "Try it out" button next to the endpoints.
+    
+    - Parameters: If an endpoint requires parameters, Swagger UI provides input boxes for you to fill in. This could include path parameters, query strings, headers, or the body of a POST/PUT request.
+
+    - Execution: Once you've inputted all the necessary parameters, you can click the "Execute" button to make a live API call. Swagger UI will send the request to the API and display the response directly in the documentation. This includes the response code, response headers, and response body.
+
+    Models
+    Swagger documents the structure of request and response bodies using models. These models define the expected data structure using JSON schema and are extremely helpful in understanding what data to send and expect.
+
+    For details on the API, please check the tutorial on the Couchbase Developer Portal: https://developer.couchbase.com/tutorial-quickstart-flask-python/
+    """,
 )
 api.init_app(app)
 
