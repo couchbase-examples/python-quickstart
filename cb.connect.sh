926b28b488fdab55b50014debddcdd3d414fb5d0
Mark Young <14414290+markfyoung0711@users.noreply.github.com>
2024-06-26 16:30:50 -0500

    add config dir

--- cb.connect.sh
+++ cb.connect.sh
@@ -1,1 +1,5 @@
-cbsh --username myoung --connstr couchbases://cb.nfoarbbt9ex6f6f.cloud.couchbase.com --disable-tls
+#!/bin/bash
+cbsh --username myoung \
+    --connstr couchbases://cb.nfoarbbt9ex6f6f.cloud.couchbase.com \
+    --config-dir $HOME/.cbsh \
+    --disable-tls
